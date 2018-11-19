92d25d77481ac6e33dccc5d0e631d5c1cf7c7674
huangyong <huangyong@singworld.net>
2018-11-19 12:00:44 +0800

    修改插件目录查找方式

--- src/plugin.js
+++ src/plugin.js
@@ -12,12 +12,12 @@ class Plugin {
   constructor(ctx) {
     this.ctx = ctx;
   }
-  toPromise(val){
-    if(!(val instanceof Promise)){
+  toPromise(val) {
+    if (!(val instanceof Promise)) {
       return new Promise((resolve, reject) => {
         resolve(val);
       });
-    }else{
+    } else {
       return val;
     }
   }
@@ -32,15 +32,22 @@ class Plugin {
     const pluginConfig = this.ctx.config.plugins,
       pluginMap = new Map();
     if (pluginConfig && Object.keys(pluginConfig).length > 0) {
-      for(let field of Object.keys(pluginConfig)){
+      for (let field of Object.keys(pluginConfig)) {
         let plugin = pluginConfig[field],
           envOpen = this._inspectEnvOpen(plugin);
         if (plugin.enable && envOpen) {
-          let pluginPackage = require(path.resolve('./node_modules/', plugin.package));
-          if(typeof pluginPackage === 'function'){
+          let pluginPackage = null;
+          try {
+            let dirname = path.dirname(require.main.filename);
+            let pluginpath = path.resolve(dirname, './node_modules/', plugin.package);
+            pluginPackage = require(pluginpath);
+          } catch (error) {
+            console.log(error);
+          };
+          if (typeof pluginPackage === 'function') {
             let res = await catchErr(this.toPromise(pluginPackage(this.ctx, plugin.config)));
             // console.warn('----------', field);
-            if(res.data) pluginMap.set(field, res.data);
+            if (res.data) pluginMap.set(field, res.data);
           }
         } else {
           console.warn(`plugin：[${field}] is not enable or env incompatible current NODE_ENV`)
