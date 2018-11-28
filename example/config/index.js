5c375ac7b8bd470d723aabba1dd5db7fb80910f9
yuronghui <yuronghui@singworld.net>
2018-11-28 11:57:43 +0800

    no message

--- example/config/index.js
+++ example/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
 	projectName: 'test_api',  //项目名
 	version: '20181016', //版本号
 	secretKey: 'woodnode', //私密串
-  isDebug: true, //是否开启调试模式
+  env: 'development',
 	errorCode: {}, //错误码
 	plugins,
 	defaultDB: 'mongodb',
